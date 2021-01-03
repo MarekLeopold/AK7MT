d88e17a83f5222aaf632c76640712b9166a2af0c
Marek Leopold <m_leopold@utb.cz>
2021-01-03 09:33:47 +0100

    Menu

--- app/src/main/java/com/example/ak7mt/MainActivity.kt
+++ app/src/main/java/com/example/ak7mt/MainActivity.kt
@@ -2,10 +2,37 @@ package com.example.ak7mt
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.example.ak7mt.fragments.HodinyFragment
+import com.example.ak7mt.fragments.PocasiFragment
+import com.example.ak7mt.fragments.PoznamkyFragment
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val hodinyFragment = HodinyFragment()
+        val pocasiFragment = PocasiFragment()
+        val poznamkyFragment = PoznamkyFragment()
+
+        makeCurrentFragment(pocasiFragment)
+
+        bottom_navigation.setOnNavigationItemSelectedListener {
+            when(it.itemId){
+                R.id.pocasi -> makeCurrentFragment(pocasiFragment)
+                R.id.Poznamky -> makeCurrentFragment(poznamkyFragment)
+                R.id.Hodiny -> makeCurrentFragment(hodinyFragment)
+            }
+            true
+        }
     }
+
+    private fun makeCurrentFragment(fragment: Fragment) =
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.fl_wrapper, fragment)
+            commit()
+        }
+
 }
